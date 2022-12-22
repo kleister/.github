df010aed2c7f00b9f3b1d782f29ecde240c702ab
Thomas Boerger <thomas@webhippie.de>
2022-12-22 19:40:00 +0100

    chore: update formatting with editorconfig, add flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "Nix flake for development";
+
+  inputs = {
+    nixpkgs = {
+      url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    };
+
+    utils = {
+      url = "github:numtide/flake-utils";
+    };
+  };
+
+  outputs = { self, nixpkgs, utils }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            terraform
+            ruby_3_1
+            shellcheck
+          ];
+        };
+      }
+    );
+}
